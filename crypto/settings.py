4e24f41e75a0fda9a30c511a0abdddeafbd49fdf
parthishere <parthishere1234@gmail.com>
2021-11-29 23:53:08 +0530

    changes

--- crypto/settings.py
+++ crypto/settings.py
@@ -27,7 +27,7 @@ if os.path.isfile(dotenv_file):
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-maeowjwpuk+-lypwi_dr_!46bxub030$1@ka2a@ne3$hn8&b#v'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
