ec1a9f7c39dc503e7b5b0263ef3ca0d5fb600712
parthishere <parthishere1234@gmail.com>
2021-11-29 18:35:03 +0530

    collectstatic

--- app/views.py
+++ app/views.py
@@ -6,13 +6,16 @@ from .models import CryptoModel, ContractTradeModel
 from .forms import CryptoForm
 
 def home(request):
-    form = CryptoForm(request.POST or None)
     context = {}
-    context['form'] = form
-    lated_range = CryptoModel.objects.filter('-id')[0]
-    context['data'] = lated_range
-    if form.is_valid and request.POST:
-        ins = form.save()
-        context['ins'] = ins
-        return redirect('home')
+    if request.user.is_admin():
+        form = CryptoForm(request.POST or None)
+        context['form'] = form
+        lated_range = CryptoModel.objects.filter('-id')[0]
+        context['data'] = lated_range
+        if form.is_valid and request.POST:
+            ins = form.save()
+            context['ins'] = ins
+            return redirect('home')
+    else:
+        context['data'] = "Not Authenticated"
     return render(request, "index.html", context=context)
--- crypto/settings.py
+++ crypto/settings.py
@@ -16,6 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -122,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
